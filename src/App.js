57e86209e00e96a723631f767e85d0a47905534e
DESKTOP-6FLLH9U\503 <idiotpi3rrot@gmail.com>
2023-06-08 10:42:27 +0900

    React Basic ~p122: CRUD/U

--- src/App.js
+++ src/App.js
@@ -70,6 +70,8 @@ function Create (props) {
 }
 
 function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
   return <article>
     <h2>Update</h2>
       <form onSubmit={event=>{
@@ -78,8 +80,13 @@ function Update(props) {
         const body = event.target.body.value;
         props.onUpdate(title, body);
       }}>
-        <p><input type="text" name="title" placeholder="title" value={props.title} /></p>
-        <p><textarea name="body" placeholder="body" value={props.body}></textarea></p>
+        <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+          console.log(event.target.value);
+          setTitle(event.target.value);
+        }}/></p>
+        <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+          setBody(event.target.value);
+        }}></textarea></p>
         <p><input type="submit" value="Update"></input></p>
       </form>
   </article>
@@ -135,8 +142,18 @@ function App() {
       }
     }
     content=<Update title={title} body={body} onUpdate={(title, body)=>{
-
+      console.log(title, body);
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body}
+      for (let i = 0; i < newTopics.length; i++) {
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
     }}></Update>
+    
   }
 
   return (
